f0a7e2de65e80dd2c5b32710d43b69539142c7e0
Bellmere <therise00@gmail.com>
2023-03-18 22:35:09 +0200

    add contacts & logic

---
+++ index.js
@@ -0,0 +1,37 @@
+const argv = require("yargs").argv;
+
+const contactsOperations = require('./contacts');
+const { listContacts, getContactById, removeContact, addContact } = contactsOperations;
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case "list":
+        const contacts = await listContacts();
+        console.log(contacts);
+        break;
+  
+      case "get":
+        const contact = await getContactById(id);
+        if (!contact) {
+            throw new Error(`Contact with such id=${id} not found`);
+        }
+        console.log(contact);
+        break;
+  
+      case "add":
+        const newContact = await addContact(name, email, phone);
+        console.log(newContact);
+        break;
+  
+      case "remove":
+        const removedContact = await removeContact(id);
+        console.log(removedContact);
+        break;
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  }
+
+  invokeAction(argv);
+
